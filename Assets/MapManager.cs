83f17c61d2f6d4c70338222def11082666f3c2ff
Nick <ndplay@ymail.com>
2023-08-31 13:47:21 +0400

    Spawnings

--- Assets/MapManager.cs
+++ Assets/MapManager.cs
@@ -6,27 +6,39 @@ public class MapManager : MonoBehaviour
 {
     public GameObject foodPrefab;
     public float spawnInterval = 2f;
-    public float cameraHeight;
-    public float cameraWidth;
-
+    public int preSpawnCount = 100; // Amount of food to spawn at the beginning
+    public float mapWidth = 100f;  // Size of the map width
+    public float mapHeight = 100f; // Size of the map height
 
     void Start()
     {
+        PreSpawnFood();
         StartCoroutine(SpawnFood());
-        cameraHeight = Camera.main.orthographicSize * 2;
-        cameraWidth = cameraHeight * Camera.main.aspect;
+    }
+
+    // Function to pre-spawn food
+    private void PreSpawnFood()
+    {
+        for (int i = 0; i < preSpawnCount; i++)
+        {
+            SpawnSingleFood();
+        }
+    }
+
+    private void SpawnSingleFood()
+    {
+        float randomX = Random.Range(-mapWidth / 2, mapWidth / 2);
+        float randomY = Random.Range(-mapHeight / 2, mapHeight / 2);
+
+        GameObject obj = Instantiate(foodPrefab, new Vector2(randomX, randomY), Quaternion.identity);
+        obj.transform.parent = transform;
     }
 
     IEnumerator SpawnFood()
     {
         while (true)
         {
-            float randomX = Random.Range(-cameraWidth / 2, cameraWidth / 2);
-            float randomY = Random.Range(-cameraHeight / 2, cameraHeight / 2);
-
-
-            Instantiate(foodPrefab, new Vector2(randomX, randomY), Quaternion.identity);
-
+            SpawnSingleFood();
             yield return new WaitForSeconds(spawnInterval);
         }
     }
