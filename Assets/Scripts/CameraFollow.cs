f390e4dba594d01d6e025473034a17d38fe62581
Nick <ndplay@ymail.com>
2023-11-10 20:01:16 +0400

    Update Horseman.prefab
    Update UniversalRP.asset
    
    Additions

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -1,20 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
 
 public class CameraFollow : MonoBehaviour
 {
     public Transform target;
     public float smoothSpeed = 0.125f;
+    public float zoomSpeed = 0.125f;
     public Vector3 offset;
-    public float minZoom, maxZoom;
-
+    public int minPixelsPerUnit, maxPixelsPerUnit;
 
     private float initialZ;
+    public PixelPerfectCamera pixelPerfectCamera;
 
     private void Start()
     {
         initialZ = transform.position.z;
+        pixelPerfectCamera = GetComponent<PixelPerfectCamera>();
     }
 
     void FixedUpdate()
@@ -24,6 +27,16 @@ public class CameraFollow : MonoBehaviour
         smoothedPosition.z = initialZ;  // Ensure Z remains the same
         transform.position = smoothedPosition;
 
-        // Zoom effect: TODO
+        // Pixel Perfect camera zooming controls
+        if (pixelPerfectCamera)
+        {
+            float scroll = Input.GetAxis("Mouse ScrollWheel");
+            pixelPerfectCamera.assetsPPU += Mathf.RoundToInt(scroll * zoomSpeed);
+            pixelPerfectCamera.assetsPPU = Mathf.Clamp(pixelPerfectCamera.assetsPPU, minPixelsPerUnit, maxPixelsPerUnit);
+        }
+        else
+        {
+            Debug.LogError("PixelPerfectCamera component not found!");
+        }
     }
 }
